3699047897d90c786edeb4b3cce249c1dbd573fa
Joan Bosch Pons <jbosch@adiquimica.com>
2020-11-18 23:32:28 +0100

    First commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+		apiKey: "AIzaSyA0YOAtV8WfVdlgLGXcMzy6YiJewA76tyI",
+		authDomain: "boschponsjoan.firebaseapp.com",
+		databaseURL: "https://boschponsjoan.firebaseio.com",
+		projectId: "boschponsjoan",
+		storageBucket: "boschponsjoan.appspot.com",
+		messagingSenderId: "865426034196",
+		appId: "1:865426034196:web:baf1f9c5da794f8cdda101",
+		measurementId: "G-FKRTXDFF0Y"
+	  }, 
+};
